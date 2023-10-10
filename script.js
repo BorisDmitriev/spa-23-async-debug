752fd33e9f9358bb7e0527958f19ff8da1b38c0e
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-10 13:43:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const userList = ['Maxim', 'Necip', 'Shaniqua', 'Lucas', 'Adam']
+
+userList.forEach((user, index) => {
+    renderUser(user, index)
+});
+
+function renderUser(name, index) {
+    console.log(`${name} ${index} `, userList)
+}
